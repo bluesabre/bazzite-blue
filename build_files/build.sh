4e1f36f9626a799abbe096f38520a084318418e0
Sean Davis <sean@bluesabre.org>
2025-12-25 09:21:01 -0500

    Install 1Password

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y 1password 
 
 # Use a COPR Example:
 #
@@ -19,6 +18,7 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
-
 systemctl enable podman.socket
+
+# Install 1password
+/ctx/scripts/install-1password.sh
