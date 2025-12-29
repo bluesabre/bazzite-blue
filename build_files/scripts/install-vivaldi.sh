d4f65d851e641dc65cb2e7cf3c665149a29af130
Sean Davis <sean@bluesabre.org>
2025-12-29 15:19:57 -0500

    Revert "Experiment with immutable /opt"
    This reverts commit cae50e57d2fe60f274e1dcb04d7555c75e3c2377.

--- build_files/scripts/install-vivaldi.sh
+++ build_files/scripts/install-vivaldi.sh
@@ -20,5 +20,14 @@ rpm --import https://repo.vivaldi.com/archive/linux_signing_key.pub
 # Now let's install the packages.
 dnf5 install -y vivaldi-stable
 
+# This places the Vivaldi contents in an image safe location
+mv /var/opt/vivaldi /usr/lib/vivaldi # move this over here
+
+# Register path symlink
+# We do this via tmpfiles.d so that it is created by the live system.
+cat >/usr/lib/tmpfiles.d/vivaldi.conf <<EOF
+L  /opt/vivaldi  -  -  -  -  /usr/lib/vivaldi
+EOF
+
 # Disable the yum repo (updates are baked into new images)
 sed -i "s@enabled=1@enabled=0@" /etc/yum.repos.d/vivaldi.repo
