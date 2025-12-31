8d0f5da9b8a87ae748e9477a9d1b5fe18bc08c7c
Sean Davis <sean@bluesabre.org>
2025-12-31 04:46:00 -0500

    Install Inter font

--- build_files/scripts/install-fonts.sh
+++ build_files/scripts/install-fonts.sh
@@ -4,6 +4,10 @@ set ${SET_X:+-x} -eou pipefail
 
 echo "Installing Fonts"
 
-dnf5 install -y mscore-fonts-all xorg-x11-font-utils cabextract fontconfig
+dnf5 install -y mscore-fonts-all \
+    xorg-x11-font-utils \
+    cabextract \
+    fontconfig \
+    rsms-inter-fonts
 
 rpm -i --nodigest https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
