bf4b30e1dae274122c3d966bc31d39e665164611
Sean Davis <sean@bluesabre.org>
2025-12-31 04:56:10 -0500

    Install Ubuntu font

--- build_files/scripts/install-fonts.sh
+++ build_files/scripts/install-fonts.sh
@@ -4,10 +4,15 @@ set ${SET_X:+-x} -eou pipefail
 
 echo "Installing Fonts"
 
+dnf5 -y copr enable atim/ubuntu-fonts
+
 dnf5 install -y mscore-fonts-all \
     xorg-x11-font-utils \
     cabextract \
     fontconfig \
-    rsms-inter-fonts
+    rsms-inter-fonts \
+    ubuntu-family-fonts
 
 rpm -i --nodigest https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
+dnf5 -y copr disable atim/ubuntu-fonts
